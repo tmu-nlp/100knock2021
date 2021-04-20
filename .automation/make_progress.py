5048adaa2159d09d8f0bcfa2e130e9ef5841a5f5
kyunashige <kyuna.b23@gmail.com>
2021-04-20 13:19:51 +0900

    Add update.yml

---
+++ .automation/make_progress.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+from pprint import pprint
+from typing import List
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+
+class User:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+        self.progress = [0] * 10
+
+
+def get_progress() -> List[User]:
+    cur = Path("../")
+    users = list(
+        filter(lambda x: x.is_dir() and x.name not in IGNORE, sorted(cur.iterdir()))
+    )
+
+    progress = list()
+    # user ごとの progress を取得する
+    for user in users:
+        u = User(user.name, user)
+        for chap, max_cnt in zip(range(CHAPTER), QUESTIONS):
+            # user/chapterXX の path (章だけ 1-indexed なので num+1)
+            chapter_path = Path(user / f"chapter{chap+1:02d}")
+            # user/chapterXX に含まれる .py ファイルと .sh ファイルの数をカウント
+            py_file_cnt = len(list(chapter_path.glob("*.py")))
+            sh_file_cnt = len(list(chapter_path.glob("*.sh")))
+            # 問題数は max_cnt が上限で、それ以上のファイル数が含まれる場合は max_cnt にする
+            solved_cnt = min(py_file_cnt + sh_file_cnt, max_cnt)
+            u.progress[chap] = solved_cnt
+        progress.append(u)
+
+    return progress
+
+
+def plot_progress(users: np.array, scores: np.array):
+    # 描画されるグラフのサイズを指定
+    plt.figure(figsize=(8, 6))
+
+    # 各章ごとに棒グラフを積み上げていく
+    for chap in range(CHAPTER):
+        label = f"chapter {chap+1:02d}"
+        bottom = np.sum(scores[:, :chap], axis=1)
+        plt.bar(
+            users,
+            scores[:, chap],
+            bottom=bottom,
+            align="center",
+            tick_label=users,
+            label=label,
+        )
+
+    # グラフの設定
+    plt.xticks(rotation=30, fontsize=10)
+    # 縦軸のラベルを 10 問刻みにする
+    whole = sum(QUESTIONS)
+    plt.ylim(0, whole)
+    plt.yticks(np.arange(0, whole + 1, 10))
+    # 凡例をグラフの外側に表示する
+    plt.legend(bbox_to_anchor=(1.28, 1.0))
+    plt.subplots_adjust(right=0.8)
+
+    # グラフを書き出す
+    plt.savefig("progress.png")
+
+
+def main():
+    data = get_progress()
+    users = np.array([user.name for user in data])
+    scores = np.array([user.progress for user in data])
+
+    plot_progress(users, scores)
+
+
+if __name__ == "__main__":
+    sns.set()
+    # 章数と各章の問題数
+    CHAPTER, QUESTIONS = 10, [10] * 10
+    # progress bar に表示しないディレクトリ名
+    IGNORE = [".git", ".github", "kiyuna", "tomoshige"]
+
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # 100knock2021
+
+小町研2021年度新入生向け勉強会として、言語処理100本ノックに取り組みます。  
+教材URL: https://nlp100.github.io/ja/
+
+毎週1章分（10問）解いてください。  
+勉強会のときに1人1問ずつ自分のコードを説明してもらいます。  
+
+## usage
+
+初回はこのレポジトリを clone してください。
+
+```
+$ git clone https://github.com/tmu-nlp/100knock2021.git
+```
+
+コードを書いたら remote repository に push してください。
+チャプター名、ファイル名はすべて二桁の数字にしてください（例: `chapter01`, `knock00.py`）。
+```
+$ git pull
+$ git add ./<user_name>/chapter##/knockXX.py
+$ git commit -m 'your message'
+$ git push origin master
+```
+
+## 注意事項
+
+新入生はできるだけ Python3 系で書いてください。
+わからないところは**積極的**に TA か研究室の人に聞いてください。
+**他の人のディレクトリを変更することは絶対にやめてください。**
+（他の人のコードを閲覧したい場合は、Web サイト上から閲覧してください。）
+chapter##/knockXX.py のフォルダ名とファイル名を間違えると進捗グラフに反映されません。
+
+## みんなの進捗
+
+![progress](progress.png)
+
+
+# 100knock2021 (English) 
+
+We will work NLP 100 knockings as the study meeting for the newcomers in 2021. 
+Let's solve the 10 questions every week.
+TA and participants review your code.  
+
+You should clone this repository the first time.
+```
+$ git clone https://github.com/tmu-nlp/100knock2021.git
+```
+
+"##" and "XX" mean "two-digit question number". (e.g. `chapter01`, `knock00.py`)
+```
+$ git pull
+$ git add ./<user_name>/chapter##/knockXX.py
+$ git commit -m 'your message'
+$ git push origin master
+```
+
+## Notes
+
+Please use Python3.  
+Please ask TA's helps **positively**.  
+Please commit your script and picture only. (Don't commit the other files such as text file because file size of the output will be gradually larger.)  
+**Don't change the others' directories.**  
+Teaching materials of this meeting are written in Japanese, so some participants need to translate them.

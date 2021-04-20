a57fe8735aba1840a3e1b7f5360028ab162e7f00
KazumaKobayash <kazuma.kobayashi.3t@gmail.com>
2021-04-20 18:31:30 +0900

    solve chapter00

--- kazuma/chapter00/knockA.py
+++ kazuma/chapter00/knockA.py
@@ -1,1 +1,5 @@
-input_data = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+input_arr = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+a = {}
+for i in range(0,len(input_data)):
+    a[i] = input_arr[i]
+print(a)

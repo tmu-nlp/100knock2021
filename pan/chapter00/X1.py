cb7c1c557a76fbf2747983aeb5a48bfbbc860d3a
pxm427 <78017600+pxm427@users.noreply.github.com>
2021-04-26 22:43:56 +0900

    Update X1.py

--- pan/chapter00/X1.py
+++ pan/chapter00/X1.py
@@ -1,2 +1,15 @@
-x = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+x = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 print(dict(enumerate(x)))
+
+
+
+
+
+#{0: 'Mon', 1: 'Tue', 2: 'Wed', 3: 'Thu', 4: 'Fri', 5: 'Sat', 6: 'Sun'}
+
+#enumerate(sequence,[startindex=0])
+#for example
+#start    sequence[0]
+#start+1  sequence[1]
+#start+2  sequence[2]
+#......

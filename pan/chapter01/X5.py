cd17b14472952a25c2d5d60df0e240b5a6be9771
pxm427 <78017600+pxm427@users.noreply.github.com>
2021-04-27 01:52:00 +0900

    Update X5.py

--- pan/chapter01/X5.py
+++ pan/chapter01/X5.py
@@ -1,1 +1,14 @@
 ##Implement a function that obtains n-grams from a given sequence object (e.g., string and list). Use this function to obtain word bi-grams and letter bi-grams from the sentence “I am an NLPer”
+def n_gram(s, n, char_level=False):
+    if char_level:
+        inputs = [c for c in s]
+    else:
+        inputs = s.split(' ')
+
+    return ['|'.join(inputs[i - n: i]) for i in range(n, len(inputs) + 1)]
+
+
+if __name__ == '__main__':
+    s = 'I am an NLPer'
+    print('Word level bi-gram:', n_gram(s, 2))
+    print('Character level bi-gram:', n_gram(s, 2, char_level=True))

141f655b9abf31460005f6fe5161d0e9b0b3f30a
pxm427 <78017600+pxm427@users.noreply.github.com>
2021-04-27 01:08:38 +0900

    Create X5.py

---
+++ pan/chapter01/X5.py
@@ -0,0 +1,1 @@
+##Implement a function that obtains n-grams from a given sequence object (e.g., string and list). Use this function to obtain word bi-grams and letter bi-grams from the sentence “I am an NLPer”

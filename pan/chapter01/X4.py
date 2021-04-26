c5e5ebeb12e2f1a3dd81e0663729281df5540eaf
pxm427 <78017600+pxm427@users.noreply.github.com>
2021-04-27 01:51:33 +0900

    Update X4.py

--- pan/chapter01/X4.py
+++ pan/chapter01/X4.py
@@ -1,2 +1,17 @@
 ##Split the sentence “Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can”. into words, and extract the first letter from the 1st, 5th, 6th, 7th, 8th, 9th, 15th, 16th, 19th words and the first two letters from the other words. Create an associative array (dictionary object or mapping object) that maps from the extracted string to the position (offset in the sentence) of the corresponding word.
-sentence = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
+
+#sentence = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
+s = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. ' \
+    'New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
+indeces = [1, 5, 6, 7, 8, 9, 15, 16, 19]
+indeces = [index - 1 for index in indeces]
+words = s.split(' ')
+
+res = {}
+for (i, word) in enumerate(words):
+    if i in indeces:
+        res[word[:1]] = i + 1
+        indeces.pop(0)
+    else:
+        res[word[:2]] = i + 1
+print(res)

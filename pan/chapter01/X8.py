20fa2f2f2e01550fe144975a0688961bb53954d4
pxm427 <78017600+pxm427@users.noreply.github.com>
2021-04-27 01:43:57 +0900

    Create X8.py

---
+++ pan/chapter01/X8.py
@@ -0,0 +1,5 @@
+##Implement a function cipher that converts a given string with the specification:
+
+##Every alphabetical lowercase letter c is converted to a letter whose ASCII code is (219 - [the ASCII code of c])
+##Keep other letters unchanged
+##Use this function to cipher and decipher an English message.

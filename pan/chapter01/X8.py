9a77d63ec800eb90fc15f073a4d280a900eaaba1
pxm427 <78017600+pxm427@users.noreply.github.com>
2021-04-27 01:49:52 +0900

    Update X8.py

--- pan/chapter01/X8.py
+++ pan/chapter01/X8.py
@@ -3,3 +3,16 @@
 ##Every alphabetical lowercase letter c is converted to a letter whose ASCII code is (219 - [the ASCII code of c])
 ##Keep other letters unchanged
 ##Use this function to cipher and decipher an English message.
+
+def encode(s):
+    return ''.join([chr(219 - ord(c)) if 'a' <= c <= 'z' else c for c in s])
+
+
+if __name__ == '__main__':
+    s = 'I am a NLPer.'
+    s_encode = encode(s)
+    s_decode = encode(s_encode)
+
+    print('Original:', s)
+    print('Encode:', s_encode)
+    print('Decode:', s_decode)

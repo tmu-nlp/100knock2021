3e920bf2c394a2e7719f5f4f2eeba9e10dcf0e08
pxm427 <78017600+pxm427@users.noreply.github.com>
2021-04-26 23:09:14 +0900

    Create X2.py

---
+++ pan/chapter00/X2.py
@@ -0,0 +1,30 @@
+#method1:just plus them
+x = [0,1]
+y = [2,3]
+z = [4,5]
+h = x + y + z
+print(h)
+
+#method2:take out all the elements and put them in a new list
+x = [0,1]
+y = [2,3]
+z = [4,5]
+new = []
+for item in x:
+  new.append(item)
+for item in y:
+  new.append(item)
+for item in z:
+  new.append(item)
+print(new)
+
+#method3:plus elements of the next list
+x = [0,1]
+y = [2,3]
+z = [4,5]
+x.extend(y)
+x.extend(z)
+print(x)
+
+
+#list together

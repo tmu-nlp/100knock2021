c2945982b40a88987c2bff9bbb3636b61b3e9022
Michitka Nakatsuji <m391gub@gmail.com>
2021-04-27 16:21:19 +0900

    fixed

--- nakatsuji/chapter01/knock05.py
+++ nakatsuji/chapter01/knock05.py
@@ -2,6 +2,5 @@ def n_gram(n, S):
     return [S[idx:idx + n] for idx in range(len(S) - n + 1)]
 
 S = "I am an NLPer"
-for i in range(1, 4):
-    print(n_gram(i, S))
-    print(n_gram(i, S.split(' ')))
+print(n_gram(2, S))
+print(n_gram(2, S.split(' ')))

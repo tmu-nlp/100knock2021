666482f59885c9494687e4fc42b94aa17e6eeb2e
suzdai <s.daisuke.2000@docomo.ne.jp>
2021-05-14 15:58:26 +0900

    chapter01

--- suzuki/chapter01/chapter01/09.py
+++ suzuki/chapter01/chapter01/09.py
@@ -22,4 +22,4 @@ def weird(text):
 
 Input = "I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
 
-print(' '.join(weird(Input)))
+print(' '.join(weird(Input)))#print

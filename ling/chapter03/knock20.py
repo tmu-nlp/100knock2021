56900f40b644003b5579e91eadf60faa0f98fecd
Ling zhidong <note980869266@gmail.com>
2021-05-17 01:27:12 +0900

    edited knock20

--- ling/chapter03/knock20.py
+++ ling/chapter03/knock20.py
@@ -1,8 +1,8 @@
 import json
 
-with open('./jawiki-country.json','r') as f:
+with open('./jawiki-country.json','r',encoding='utf-8') as f:
     for line in f:
         line=json.loads(line)
         if line['title']=='イギリス':
-            text=line
+            text=line['text']
     print(text)

5a950bb4eabd669ee2e7a85bed4f5ac29b1fa951
suzdai <s.daisuke.2000@docomo.ne.jp>
2021-05-25 01:29:59 +0900

    added 04

---
+++ suzuki/chapter04/knock34.py
@@ -0,0 +1,21 @@
+#名詞の連接（連続して出現する名詞）を最長一致で抽出せよ．
+
+from knock30 import make_morpheme
+
+f = open('neko.txt.mecab', 'r')
+m = make_morpheme(f)
+ans = []
+second = []
+count = []
+
+for morph in m:
+    if morph['pos'] == '名詞':
+        count.append(morph['surface'])
+        if len(count) > len(ans):
+            second = ans
+            ans = count
+    else:
+        count = []
+
+print(' '.join(second))
+print(' '.join(ans))

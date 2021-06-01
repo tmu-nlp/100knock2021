b0ed18139ddc28ba7c590d886e7d05eae9280107
hkikuchi <hkikuchi@student.42tokyo.jp>
2021-06-01 12:54:41 +0900

    added new func

--- HinataKikuchi/chapter05/knock.py
+++ HinataKikuchi/chapter05/knock.py
@@ -11,6 +11,15 @@ class Morph:
 		self.pos1 = word[5]
 	def print_self(self):
 		print(self.surface, self.base, self.pos, self.pos1)
+	def check_pos(self, pos:str):
+		if pos == self.pos:
+			return 1
+		return 0
+	def check_pos1(self, pos1:str):
+		if pos1 == self.pos1:
+			return 1
+		return 0
+
 
 class Chunk:
 	morphs : list
@@ -31,4 +40,25 @@ class Chunk:
 			if morph.pos == pos:
 				return 1
 		return 0
-
+	def check_pos1(self, pos1:str) -> int:
+		for morph in self.morphs:
+			if morph.pos1 == pos1:
+				return 1
+		return 0
+	def check_surface(self, surface:str) -> int:
+		for morph in self.morphs:
+			if morph.surface == surface:
+				return 1
+		return 0
+	def XYphrase(self, XorY:str) -> str:
+		XYphrase = ''
+		flag = 0
+		for morph in self.morphs:
+			if morph.pos == '名詞' and flag == 0:
+				XYphrase += XorY
+				flag += 1
+			elif morph.pos == '名詞' and flag >= 1:
+				continue
+			else:
+				XYphrase += morph.surface
+		return XYphrase

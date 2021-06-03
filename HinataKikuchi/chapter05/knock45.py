04faa409284c34a7b7b2006182d8c56ff1ea4c47
hkikuchi <hkikuchi@student.42tokyo.jp>
2021-06-03 10:35:57 +0900

    resolved 45

--- HinataKikuchi/chapter05/knock45.py
+++ HinataKikuchi/chapter05/knock45.py
@@ -3,13 +3,19 @@ from knock41 import phrases
 def print_case(phrase):
 	for ch in phrase:
 		if ch.check_pos('動詞') == 1 and len(ch.srcs) != 0:
-			print(ch.phrase, end = '\t')
+			for idx, ch_morph in enumerate(ch.morphs):
+				if idx < len(ch.morphs):
+					print(ch_morph.surface, end = '')
+				else:
+					print(ch_morph.base, end='\t')
 			for idx in ch.srcs:
 				if phrase[idx].check_pos('助詞') == 1:
 					for morph in phrase[idx].morphs:
 						if morph.check_pos('助詞') == 1:
-							print(morph.surface, end = '\t')
+							print(morph.surface, end = ' ')
 			print()
+
+# print_case(phrases[1])
 
 for phrase in phrases:
 	print_case(phrase)

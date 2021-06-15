bbfcba4c8834b5fe7e84067589adfe436e34966c
suzdai <s.daisuke.2000@docomo.ne.jp>
2021-06-15 15:33:59 +0900

    fixed chapter07

--- suzuki/chapter07/test.py
+++
@@ -1,7 +0,0 @@
-line = ['hello i am daisuke suzuki', 'hello i am john cage', 'i wanna quit lab', 'ninhao wo jao lingmu dayue']
-
-for l in line:
-    L = l.strip().split(' ')
-    if 'hello' in L[0] or 'ninhao' in L[0]:
-        print(L)
-        print(L[3] + ' ' + L[4])

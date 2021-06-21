36e125423aebae24bb95e08265c19a947f032449
Kousuke Takaki <u7693@protonmail.com>
2021-06-21 21:47:41 +0000

    chapter07

--- takaki/chapter07/knock64.py
+++ takaki/chapter07/knock64.py
@@ -3,22 +3,25 @@ from gensim.models import KeyedVectors
 MODEL_PATH = './tmp/GoogleNews-vectors-negative300.bin.gz'
 MODEL = KeyedVectors.load_word2vec_format(fname=MODEL_PATH, binary=True)
 
-l = []
-
 with open('./tmp/questions-words.txt', 'r') as f:
     lines = f.readlines()
 
+result = []
+category = ''
+
 for line in lines:
     words = line.split()
-    if len(words) >= 3:
-        w, v = MODEL.most_similar(
+    if words[0] == ':':
+        category = words[1]
+    else:
+        word, cos = MODEL.most_similar(
             positive=[words[1], words[2]],
             negative=[words[0]],
             topn=1
         )[0]
-        l.append(' '.join(words + [w, str(v)]) + '\n')
-    else:
-        l.append(line)
+        result.append(
+            ' '.join([category] + words + [word, str(cos)]) + '\n'
+        )
 
 with open('./tmp/knock64.txt') as f:
-    f.writelines(l)
+    f.writelines(result)

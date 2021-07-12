86d1320b7a9db0fc091e97dd73fa97ca674ff44a
masamune0103 <easymcy0103@gmail.com>
2021-07-12 23:06:41 +0900

    chapter10

---
+++ masamune/chapter10/knock91.py
@@ -0,0 +1,23 @@
+'''
+mkdir checkpoints
+mkdir checkpoints/kftt.ja-en
+'''
+#train
+import os
+
+os.system(
+"fairseq-train data-bin/kftt.ja-en \
+    --save-dir checkpoints/kftt.ja-en/ \
+    --arch lstm --share-decoder-input-output-embed \
+    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
+    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+    --dropout 0.3 --weight-decay 0.0001 \
+    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+    --max-tokens 4096 \
+    --max-epoch 5 \
+    --eval-bleu \
+    --eval-bleu-detok moses \
+    --eval-bleu-remove-bpe \
+    --eval-bleu-print-samples \
+    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric"
+)
---
+++ masamune/chapter10/knock94.py
@@ -0,0 +1,34 @@
+'''
+!mkdir 94
+
+#beam size = 1
+!fairseq-interactive \
+  --path checkpoints/kftt.ja-en/checkpoint_best.pt \
+  --beam 1 data-bin/kftt.ja-en/ \
+  < kftt-data-1.0/data/tok/kyoto-dev.ja | grep '^H' | cut -f3 > 94/beam1.out
+!fairseq-score --sys 94/beam1.out --ref kftt-data-1.0/data/tok/kyoto-dev.en
+#出力
+["Namespace(ignore_case=False, order=4, ref='kftt-data-1.0/data/tok/kyoto-dev.en', sacrebleu=False, sentence_bleu=False, sys='94/beam1.out')",
+ 'BLEU4 = 7.49, 31.1/10.8/4.5/2.1 (BP=1.000, ratio=1.331, syslen=32367, reflen=24309)']
+
+#beam size = 5
+!fairseq-interactive \
+  --path checkpoints/kftt.ja-en/checkpoint_best.pt \
+  --beam 5 data-bin/kftt.ja-en/ \
+  < kftt-data-1.0/data/tok/kyoto-dev.ja | grep '^H' | cut -f3 > 94/beam5.out
+!fairseq-score --sys 94/beam5.out --ref kftt-data-1.0/data/tok/kyoto-dev.en
+#出力
+["Namespace(ignore_case=False, order=4, ref='kftt-data-1.0/data/tok/kyoto-dev.en', sacrebleu=False, sentence_bleu=False, sys='94/beam5.out')",
+ 'BLEU4 = 9.70, 36.5/13.4/6.0/3.0 (BP=1.000, ratio=1.137, syslen=27640, reflen=24309)']
+
+#beam size = 10
+!fairseq-interactive \
+  --path checkpoints/kftt.ja-en/checkpoint_best.pt \
+  --beam 10 data-bin/kftt.ja-en/ \
+  < kftt-data-1.0/data/tok/kyoto-dev.ja | grep '^H' | cut -f3 > 94/beam10.out
+!fairseq-score --sys 94/beam10.out --ref kftt-data-1.0/data/tok/kyoto-dev.en
+#出力
+["Namespace(ignore_case=False, order=4, ref='kftt-data-1.0/data/tok/kyoto-dev.en', sacrebleu=False, sentence_bleu=False, sys='94/beam10.out')",
+ 'BLEU4 = 9.63, 35.8/13.3/6.0/3.0 (BP=1.000, ratio=1.149, syslen=27943, reflen=24309)']
+
+'''
